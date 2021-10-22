f72fc737176a78dc82c74a4e5e7c3e0e09adfb69
liangqin.fan <liangqin.fan@gmail.com>
2021-10-22 10:33:41 +0800

    update dep versions

--- ask-cli-init.js
+++ ask-cli-init.js
@@ -4,7 +4,17 @@ const https = require('https');
 const {execute} = require('./utils/executor');
 const fs = require('fs-extra');
 const path = require('path');
+const program = require('commander');
+
 const DepensUrl = "https://raw.githubusercontent.com/patractlabs/ask-cli/main/depens.json";
+
+program
+  .option('--latest', 'to use latest version');
+
+  program.parse(process.argv);
+
+const options = program.opts();
+const useDefault = options.latest === undefined;
 
 function install_depens(depens) {
   const keys = Object.keys(depens);
@@ -29,18 +39,29 @@ function create_folders() {
 }
 
 console.log("start to install dependencies for ask! ");
-https.get(DepensUrl, (resp) => {
-  let data = '';
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
 
-  // complete response has been received.
-  resp.on('end', () => {
-    install_depens(JSON.parse(data));
-    create_folders();
-  });
+if (useDefault) {
+  let DefaultDeps = {
+    "ask-lang": "0.1.0",
+    "ask-transform": "0.0.16"
+  };
 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+  install_depens(DefaultDeps);
+  create_folders();
+} else {
+  https.get(DepensUrl, (resp) => {
+    let data = '';
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // complete response has been received.
+    resp.on('end', () => {
+      install_depens(JSON.parse(data));
+      create_folders();
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+}
